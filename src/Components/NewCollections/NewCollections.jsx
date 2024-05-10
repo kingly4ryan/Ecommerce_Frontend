2b44e1700f8cdeb6a94e2369df82ffd374be4890
Aryan Gupta <67500253+kingly4ryan@users.noreply.github.com>
2024-05-11 02:37:37 +0530

    Update NewCollections.jsx

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -8,7 +8,7 @@ const NewCollections = () => {
     const [new_collection, setNew_collection] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:4000/newcollection")
+        fetch("https://ecommerce-backend-1pbs.onrender.com/newcollection")
             .then((response) => response.json())
             .then((data) => setNew_collection(data));
     }, []);
